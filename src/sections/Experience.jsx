8b9a0e3c59607b8d2956c0463d1ffe30b57826f2
aayushmlk <mallickaayush@gmail.com>
2026-03-13 23:57:16 +0545

    exp change

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -79,7 +79,7 @@ export const Experience = () => {
             className="text-muted-foreground
            animate-fade-in animation-delay-200"
           >
-            A timeline of my professional growth, from concept to creation of scalable products.
+            A journey of professional growth, from concept to creating scalable products.
           </p>
         </div>
 
